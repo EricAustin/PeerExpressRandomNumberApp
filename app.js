a1238118d9ab84ecb638e3ab1182c6ed754de10a
Eric Austin <ewaustin@gmail.com>
2017-08-01 16:59:20 -0500

    done

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ console.log('express sourced')
 
 app.use(express.static('public'));
 
+app.get('/random', function(req, res) {
+    res.send(Math.random().toString());
+});
+
 app.listen(port, function(){
     console.log('listening on port', port);
 })
